dd8a08a6eaddb235b5ae7d1337e083b029123c37
Simon Paesler <simon.paesler@digatus.de>
2021-07-20 16:43:42 +0200

    added neo query

--- BenchmarkApp/Server/Database/Neo4J/Services/Neo4JRepository.cs
+++ BenchmarkApp/Server/Database/Neo4J/Services/Neo4JRepository.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BenchmarkApp.Server.Database.Core;
 using BenchmarkApp.Server.Database.Neo4J.Entities;
 using BenchmarkApp.Server.Database.Neo4J.Interfaces;
 using Neo4jClient;
+using Neo4jClient.Cypher;
 
 namespace BenchmarkApp.Server.Database.Neo4J.Services
 {
@@ -16,12 +18,34 @@ namespace BenchmarkApp.Server.Database.Neo4J.Services
 
         public async Task<IEnumerable<Neo4JUserEntity>> GetAllFriendsAsync(int level)
         {
-            var result = await _client.Cypher
-                .Match(@"(user:User{name: $name})-[:KNOWS]->(friend:User)")
-                .WithParam("name", Config.RootUserName)
-                .Return<Neo4JUserEntity>("friend")
-                .ResultsAsync;
             
+            // Match (user:User{name:"Max Mustermann"})-[:KNOWS]->(friend:User)
+            // -[:KNOWS]->(friend0:User)-[:KNOWS]->(friend1:User)
+            // -[:KNOWS]->(friend2:User) return friend, friend0, friend1, friend2
+            
+            
+            var queryString = @"(user:User{name: $name})-[:KNOWS]->(friend:User)";
+            var resultString = "friend";
+
+            foreach (var i in Enumerable.Range(0, level))
+            {
+                queryString += ($"-[:KNOWS]->(friend{i}:User)");
+                resultString += ($"friend{i}");
+            }
+
+            var result = await _client.Cypher
+                .Match(queryString)
+                .WithParam("name", Config.RootUserName)
+                .Return(() => new
+                {
+                    Friends = Return.As<Neo4JUserEntity>("friend"),
+                    Friends0 = Return.As<Neo4JUserEntity>("friend0"),
+                    Friends1 = Return.As<Neo4JUserEntity>("friend1"),
+                    Friends2 = Return.As<Neo4JUserEntity>("friend2"),
+                })
+                .ResultsAsync;
+
+
             return new List<Neo4JUserEntity>();
         }
 
