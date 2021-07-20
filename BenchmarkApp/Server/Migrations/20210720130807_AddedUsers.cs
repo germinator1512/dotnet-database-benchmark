9248e6ed169f42166760a4baba0a5d5fd5d3e26d
Simon Paesler <simon.paesler@digatus.de>
2021-07-20 15:09:13 +0200

    forgot file

--- BenchmarkApp/Server/Migrations/20210720130807_AddedUsers.cs
+++ BenchmarkApp/Server/Migrations/20210720130807_AddedUsers.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace BenchmarkApp.Server.Migrations
 {
-    public partial class AddedEntities : Migration
+    public partial class AddedUsers : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
